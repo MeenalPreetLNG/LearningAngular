06161102ab232693268353de477c8c4d6db53883
MeenalKaurLNG <meenalpreet.kaur@lngconsultancy.co.in>
2021-05-28 12:21:01 +0530

    Add Routing test moudle

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,48 @@
-// import { TestBed } from '@angular/core/testing';
-// import { AppComponent } from './app.component';
+import { APP_BASE_HREF } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
 
-// describe('AppComponent', () => {
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [
-//         AppComponent
-//       ], 
-//       imports: [ 
-//           RouterTestingModule 
-//         ]
-//     }).compileComponents();
-//   });
+describe('AppComponent', () => {
 
-//   it('should create the app', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.componentInstance;
-//     expect(app).toBeTruthy();
-//   });
+const routes: Routes = [
+    { path: 'welcome', component: WelcomeComponent },
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+  ];
 
-//   it(`should have as title 'learn'`, () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.componentInstance;
-//     expect(app.pageTitle).toEqual('Hill Top');
-//   });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent,
+        WelcomeComponent
+      ],
+      imports: [
+        BrowserModule,
+        HttpClientModule,
+        RouterModule.forRoot(routes)
+      ], 
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' }
+      ]
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+  
+
+  it(`should have as title 'learn'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.pageTitle).toEqual('Hill Top');
+  });
 
   
-// });
+});
---
+++ src/app/products/product-detail.guard.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ProductDetailGuard } from './product-detail.guard';
+
+describe('ProductDetailGuard', () => {
+  let guard: ProductDetailGuard;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+        imports :[RouterTestingModule]
+      });
+    guard = TestBed.inject(ProductDetailGuard);
+  });
+
+  it('should be created', () => {
+    expect(guard).toBeTruthy();
+  });
+});
